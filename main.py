1808109353d75be2065b0d70be4d7f8a9f6c0540
hasnaine ahmed dihan <dihanahmed205@gmail.com>
2025-05-18 20:41:44 +1000

    some tweaking in the code

--- main.py
+++ main.py
@@ -81,11 +81,14 @@ if query:
             embeddings,
             allow_dangerous_deserialization=True
         )
+        
+
         chain=RetrievalQAWithSourcesChain.from_llm(llm=llm, retriever=vectorstore.as_retriever())
         result=chain({"question": query}, return_only_outputs=True)
+        
         st.header("Answer:")
         st.write(result['answer'])
-        
+
         # Display the sources
         sources=result.get('sources', "")
         if sources:
